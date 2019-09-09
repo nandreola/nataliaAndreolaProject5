4bc8bdbdd674df00b501087a22f3561349f588dd
Natalia <nathy.andreola@gmail.com>
2019-09-09 16:00:06 -0400

    created Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>Developed by Natalia Andreola</p>
+                <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>          
+            </div>
+        );
+    }
+}
+
+export default Footer;
+
+
